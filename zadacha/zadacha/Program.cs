c59b9d59df279eefc41ed3643a216fccbb90c4f4
TolgaLudiq <tolga.beyti@gmail.com>
2021-02-17 09:31:15 +0200

    dobavqm tema 2 / broini sistemi

---
+++ zadacha/zadacha/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedi dvoichno chislo: ");
+            string bin = Console.ReadLine();
+            int dec = Convert.ToInt32(bin, 2);
+            Console.WriteLine("Desetichnoto: "+dec);
+            Console.WriteLine("Dvoichno: " +bin);
+        }
+    }
+}
