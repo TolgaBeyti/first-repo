a8fb11cda400ea8ca8607d275325524ac0bbe188
TolgaLudiq <tolga.beyti@gmail.com>
2021-02-24 09:58:26 +0200

    Chisleni zadachi

---
+++ Chisleni zadachi/1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете десетично число");
+            int chislo = int.Parse(Console.ReadLine());
+            string chislo2 = Convert.ToString(chislo, 2);
+            Console.WriteLine("Desetichno: " + chislo);
+            Console.WriteLine("Dvoichno: " + chislo2);
+            string chislo16 = Convert.ToString(chislo, 16);
+            Console.WriteLine("Shestnadesetichno: " + chislo16);
+        }
+    }
+}
---
+++ Chisleni zadachi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedete dvoichno chislo = ");
+            string chislo = Console.ReadLine();
+            int chislo10 = Convert.ToInt32(chislo, 2);
+
+
+
+            Console.WriteLine("Vuvedete dvoichno chislo = ");
+            string chislo2 = Console.ReadLine();
+            int chislo210 = Convert.ToInt32(chislo2, 2);
+            int sum = chislo10 + chislo210;
+            string sum2 = Convert.ToString(sum, 2);
+            Console.WriteLine(sum2);
+        }
+    }
+}
