220444372863c80a48fcb417d8d278280ed45c61
GANG GANG <78840876+TolgaLudiq@users.noreply.github.com>
2021-09-20 10:25:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace broini_sistemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = int.Parse(Console.ReadLine());
+            string chislo2 = Convert.ToString(chislo,2);
+            Console.WriteLine("dvuichnoto chislo e " + chislo2);
+        }
+    }
+}
